a4fdeae94d7ccd4157c19451fdb4430764595c9f
Eugene Sky <eugene.sky.dev@gmail.com>
2023-10-30 00:23:47 -0500

    header

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partials/header/header.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    declarations: [
+      AppComponent,
+      HeaderComponent
+    ]
   }));
 
   it('should create the app', () => {
