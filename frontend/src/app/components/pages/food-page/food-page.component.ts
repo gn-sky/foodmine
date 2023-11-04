4f993b4b54a71015532fe2c514e3b8e23791bba4
Eugene Sky <eugene.sky.dev@gmail.com>
2023-11-04 00:42:40 -0500

    CartService

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { CartService } from 'src/app/services/cart.service';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/food';
 
@@ -10,15 +11,25 @@ import { Food } from 'src/app/shared/models/food';
   styleUrls: ['./food-page.component.scss'],
 })
 export class FoodPageComponent implements OnDestroy {
-  food!: Food;  
+  food!: Food;
   private routeSubscription: Subscription;
-  
-  constructor(activatedRoute: ActivatedRoute, foodService: FoodService) {
-    this.routeSubscription = activatedRoute.params.subscribe(params => {
+
+  constructor(
+    activatedRoute: ActivatedRoute,
+    foodService: FoodService,
+    private cartService: CartService,
+    private router: Router
+  ) {
+    this.routeSubscription = activatedRoute.params.subscribe((params) => {
       if (params.id) {
-        this.food = foodService.getById(params.id);        
+        this.food = foodService.getById(params.id);
       }
-    })
+    });
+  }
+
+  addToCart() {
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
   }
 
   ngOnDestroy(): void {
