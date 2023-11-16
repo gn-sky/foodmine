0dac33342149249d974c67f8c82cd600ade46213
Eugene Sky <eugene.sky.dev@gmail.com>
2023-11-15 22:47:58 -0600

    login form

---
+++ frontend/src/app/components/pages/login-page/login-page.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginPageComponent } from './login-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TitleComponent } from '../../partials/title/title.component';
+
+describe('LoginPageComponent', () => {
+  let component: LoginPageComponent;
+  let fixture: ComponentFixture<LoginPageComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [LoginPageComponent, TitleComponent],
+      imports: [ReactiveFormsModule]
+    });
+    fixture = TestBed.createComponent(LoginPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
